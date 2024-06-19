be4b3f40a14a5e7906d9242a066e0ffad0be07b3
simplifiedcourses <brecht@simplified.courses>
2024-06-19 17:53:40 +0200

    Added initial project

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,13 @@
-import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import 'zone.js';
+import { AppComponent } from './app/app.component';
+import { Routes, provideRouter, withEnabledBlockingInitialNavigation } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+const appRoutes: Routes = [];
 
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
-  `,
-})
-export class App {
-  name = 'Angular';
-}
-
-bootstrapApplication(App);
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(),
+    provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
+  ],
+});
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/**/*.{html,ts}",
+    ],
+    darkMode: 'class',
+    theme: {
+      extend: {
+        colors: {
+          primary: {
+            500: '#0CC', // Replace with your color code
+            600: '#2CC', // Replace with your color code
+          },
+        },
+      },
+    },
+    variants: {
+      extend: {
+        ringColor: ['focus'],
+        borderColor: ['focus'],
+      },
+    },
+    plugins: [
+      require('@tailwindcss/forms'),
+    ],
+  }
+  
